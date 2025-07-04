8acc88b921d6f85edf284276adc0d080d8a3e314
Szymon Lipka <szymon.lipka@acima.com>
2025-07-04 12:16:35 +0200

    remove version spec

--- spec/migration_queries/version_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "migration_queries/version"
-
-RSpec.describe MigrationQueries::VERSION do
-  it "has a version number" do
-    expect(MigrationQueries::VERSION).to eq("0.2.1")
-  end
-end
