35874abb8e4d4b4e21a8865766a6d2ecbe7cb45b
Szymon Lipka <szymon.lipka@acima.com>
2025-07-04 11:04:24 +0200

    update gemfile

--- spec/migration_queries/version_spec.rb
+++ spec/migration_queries/version_spec.rb
@@ -4,6 +4,6 @@ require "migration_queries/version"
 
 RSpec.describe MigrationQueries::VERSION do
   it "has a version number" do
-    expect(MigrationQueries::VERSION).to eq("0.2.0")
+    expect(MigrationQueries::VERSION).to eq("0.2.1")
   end
 end
